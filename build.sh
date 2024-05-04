dc1ab08a140a96b46df305234d4426d5324d5f24
Juan Pablo Patino Lopez <juanppdev@gmail.com>
2024-05-04 22:16:42 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 python -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
-pip install -r requirements.txt
-rm -rf public
 reflex init
 reflex export --frontend-only
 unzip frontend.zip -d public
