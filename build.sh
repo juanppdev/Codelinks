375ae5b1ccbf42279bac49fc91a77a2648adb4c0
Juan Pablo Patino Lopez <fomundi.fr@gmail.com>
2024-09-08 21:19:14 +0200

    Add btn

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ python -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-rm -f public
+rm -rf public
 reflex init
 reflex export --frontend-only
 unzip frontend.zip -d public
