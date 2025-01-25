f8f989c5b5cdf78cbdd412cfe0d43872984173c1
Juan Pablo Patino Lopez <fomundi.fr@gmail.com>
2025-01-25 12:05:58 +0100

    Add new Projects

--- python_links/constants.py
+++ python_links/constants.py
@@ -20,3 +20,7 @@ COFFEE_URL="https://buymeacoffee.com/juanppdev"
 
 # Contacto
 PORTFOLIO = "https://link.mundocode.dev/portafolio"
+
+# Repositorios
+DRAGON_BALL_API="https://github.com/juanppdev/DragonBallApi"
+DRAGON_BALL_APP_KT="https://github.com/juanppdev/Dragon-Ball"
--- python_links/views/projects_links.py
+++ python_links/views/projects_links.py
@@ -6,6 +6,7 @@ import python_links.styles.styles as styles
 from python_links.styles.styles import Size as Size
 from python_links.styles.styles import Spacing as Spacing
 from python_links.components.link_button import link_button
+import python_links.constants as const
 
 def projects_links() -> rx.Component:
     return rx.vstack(
@@ -13,7 +14,20 @@ def projects_links() -> rx.Component:
             "Mis proyectos",
             size=Size.BIG.value
         ),
-        #link_button(),
+        link_button(
+            "Dragon Ball Api",
+            "Api de Dragon Ball",
+            "https://www.dragonballapi.com/static/images/Hero.webp",
+            const.DRAGON_BALL_API,
+            True
+        ),
+        link_button(
+            "Dragon Ball App",
+            "App de Dragon Ball para Android",
+            "/appKT.png",
+            const.DRAGON_BALL_APP_KT,
+            True
+        ),
 
         width="100%",
         spacing=Spacing.DEFAULT.value,
