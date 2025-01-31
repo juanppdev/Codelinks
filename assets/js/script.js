9c3e538b5fe2d151ee773564335a568dcabeb8ab
Juan Pablo Patino Lopez <fomundi.fr@gmail.com>
2025-01-31 22:17:55 +0100

    update map

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Obtiene la localización mediante ipinfo.io
-    fetch('https://ipinfo.io/json?token=7ce6f02ebdc5a2')
-        .then(response => response.json())
-        .then(data => {
-            const [lat, lng] = data.loc.split(',');
+const socket = io("https://mapacode.vercel.app/");
 
-            // Envía la ubicación al servidor Node.js en mapacode.vercel.app
-            fetch('https://mapacode.vercel.app/api/locations', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    lat: parseFloat(lat),
-                    lng: parseFloat(lng),
-                    country: data.country,
-                    id: data.ip // Usamos la IP como identificador único
-                })
-            }).then(response => {
-                if (response.ok) {
-                    console.log('Ubicación enviada correctamente');
-                } else {
-                    console.error('Error al enviar la ubicación');
-                }
-            }).catch(error => {
-                console.error('Error en la solicitud fetch:', error);
-            });
-        })
-        .catch(error => {
-            console.error('Error al obtener la localización:', error);
+  function sendLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit("sendLocation", {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
         });
-});
+      });
+    }
+  }
+
+  window.addEventListener("load", sendLocation);
+  window.addEventListener("beforeunload", () => {
+    socket.disconnect();
+  });
