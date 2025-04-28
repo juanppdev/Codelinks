9e96007f19a0192310113081d2d476418bfda29d
Juan Pablo Patino Lopez <juanppdev@gmail.com>
2025-04-28 21:17:15 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,20 +2,9 @@ python -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-
-# Solo inicializa si no existe el proyecto
-if [ ! -f rxconfig.py ]; then
-  reflex init
-fi
-
+rm -rf public
+reflex init
 reflex export
-if [ -f frontend.zip ]; then
-  rm -rf public
-  unzip frontend.zip -d public
-  rm -f frontend.zip
-else
-  echo "Error: frontend.zip no fue generado."
-  exit 1
-fi
-
+unzip frontend.zip -d public
+rm -f frontend.zip
 deactivate
